beeb6bd2308e35ef323d604c0b081e50d4f848a9
Mohamed Mbarek <med.mbarek.isi@gmail.com>
2022-12-06 10:04:48 +0100

    v1

--- server/main.py
+++ server/main.py
@@ -1,13 +1,15 @@
+import cv2
+import tensorflow as tf
 from flask import Flask, request
 from flask_cors import CORS
 from flask import render_template
-from fastai.vision.all import *
+from tensorflow.keras.models import load_model
 
 #Labeling function required for load_learner to work
 def GetLabel(fileName):
   return fileName.split('-')[0]
 
-learn = load_learner(Path('server/export.pkl')) #Import Model
+model = load_model('imageclassifier.h5') #Import Model
 app = Flask(__name__)
 cors = CORS(app) #Request will get blocked otherwise on Localhost
 
@@ -17,9 +19,12 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    img = PILImage.create(request.files['file'])
-    label,_,probs = learn.predict(img)
-    return f'{label} ({torch.max(probs).item()*100:.0f}%)'
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
+    img = cv2.imread(f.filename)
+    resize = tf.image.resize(img, (256,256))
+    yhat = model.predict(np.expand_dims(resize/255, 0))
+    return f'{yhat[0]} hahahaha%)'
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
